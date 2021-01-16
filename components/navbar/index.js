70d126f3c7a3a8b4dfae491e6efa791298db7080
David <ddropping@gmail.com>
2021-01-15 20:06:54 -0800

    Create navbar underline animation

--- components/navbar/index.js
+++ components/navbar/index.js
@@ -5,7 +5,6 @@ import styled from "styled-components";
 export const Container = styled.div``;
 
 export const Ul = styled.ul`
-  height: 60px;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -14,11 +13,28 @@ export const Ul = styled.ul`
   margin: 0;
 `;
 
+export const ItemWrapper = styled.div`
+  height: 60px;
+  padding-top: 15px;
+  :hover > .underline {
+    transition: width 0.5s;
+    width: 100%;
+  }
+`;
+
 export const Li = styled.li`
+  cursor: pointer;
   padding: 0 25px;
   @media (max-width: 576px) {
     padding: 0 15px;
   }
+`;
+
+export const Underline = styled.div`
+  margin: 0 auto;
+  height: 2px;
+  width: 0;
+  background-color: #6a8aba;
 `;
 
 const index = () => {
@@ -30,28 +46,43 @@ const index = () => {
           animate={{ y: "0", opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.5 }}
         >
-          <Li>About</Li>
+          <ItemWrapper>
+            <Li>About</Li>
+            <Underline className="underline" />
+          </ItemWrapper>
         </motion.div>
         <motion.div
           initial={{ y: "-30px", opacity: 0 }}
           animate={{ y: "0", opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.6 }}
         >
-          <Li>Skills</Li>
+          <ItemWrapper>
+            {" "}
+            <Li>Skills</Li>
+            <Underline className="underline" />
+          </ItemWrapper>
         </motion.div>
         <motion.div
           initial={{ y: "-30px", opacity: 0 }}
           animate={{ y: "0", opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.7 }}
         >
-          <Li>Projects</Li>
+          <ItemWrapper>
+            {" "}
+            <Li>Projects</Li>
+            <Underline className="underline" />
+          </ItemWrapper>
         </motion.div>
         <motion.div
           initial={{ y: "-30px", opacity: 0 }}
           animate={{ y: "0", opacity: 1 }}
           transition={{ duration: 0.5, delay: 2.8 }}
         >
-          <Li>Contact</Li>
+          <ItemWrapper>
+            {" "}
+            <Li>Contact</Li>
+            <Underline className="underline" />
+          </ItemWrapper>
         </motion.div>
       </Ul>
     </Container>
